e30c4516be7ac66eff42e0e6cf1b0efa1ae0a721
kashikichi <60090083+Pikselas@users.noreply.github.com>
2024-06-14 10:59:41 +0530

    appear the blog posts

---
+++ ts/PageFuntions.ts
@@ -0,0 +1,9 @@
+function ShowBlogPosts(query: string)
+{
+    document.getElementById("blog_itm_container").style.top = "calc(50% - 42.5%)";
+}
+
+function HideBlogPosts()
+{
+    document.getElementById("blog_itm_container").style.top = "100%";
+}
